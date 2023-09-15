618c446e297756a566225515740712c5f9ddd93c
Ashwin K N <88036631+ashwinkn2004@users.noreply.github.com>
2023-09-15 20:07:34 +0530

    Create q1.java

---
+++ Q1/q1.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String regId;
+		String stdName;
+		
+		regId = "MUT22CS256" ;
+		stdName = "ASHWIN K N" ;
+		
+		System.out.println("Name         = Ashwin K N");
+		System.out.println("Roll No      = 29");
+		
+		System.out.println("Register ID  = "+regId);
+		System.out.println("Student Name = "+stdName);
+	}
+}
+
+
